63fcdf64c523c6109e49ae29cb7f11be80db6697
muhammed-shadil <muhammedshadil220@gmail.com>
2024-08-19 20:42:38 +0530

    new effect in post

--- lib/view/screens/Home_Screen/single_post.dart
+++ lib/view/screens/Home_Screen/single_post.dart
@@ -2,16 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:trek/utils/constants.dart';
 import 'package:trek/utils/styles.dart';
 
-class SinglePost extends StatelessWidget {
+class SinglePost extends StatefulWidget {
   const SinglePost({super.key});
 
   @override
+  _SinglePostState createState() => _SinglePostState();
+}
+
+class _SinglePostState extends State<SinglePost> {
+  bool isSwapped = false;
+
+  @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.78,
-      width: MediaQuery.of(context).size.width * 0.9,
+      height: screenHeight * 0.78,
+      width: screenWidth * 0.9,
       child: Stack(
         children: [
+          // Large Image (conditionally behind or in front based on isSwapped)
+          if (!isSwapped) buildLargeImage(screenWidth, screenHeight),
+          buildSmallImage(screenWidth, screenHeight),
+          if (isSwapped) buildLargeImage(screenWidth, screenHeight),
+
+          // Text and Avatar (unchanged)
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -38,38 +54,77 @@ class SinglePost extends StatelessWidget {
               )
             ],
           ),
-          Positioned(
-              top: 70,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                      image: AssetImage(
-                        "assets/b64ac4c65983e8702efcc1024bf6b142-e1505106508525.jpg",
-                      ),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: MediaQuery.of(context).size.height * 0.6,
-              )),
-          Positioned(
-              top: 5,
-              child: Container(
-                decoration: BoxDecoration(
-                  border:
-                      Border.all(width: 5, color: constants.backgroundColor),
-                  image: const DecorationImage(
-                      image: AssetImage(
-                          "assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg")),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(25),
-                      bottomRight: Radius.circular(25),
-                      bottomLeft: Radius.circular(25)),
-                ),
-                width: 90,
-                height: 120,
-              )),
         ],
+      ),
+    );
+  }
+
+  Widget buildLargeImage(double screenWidth, double screenHeight) {
+    return AnimatedPositioned(
+      top: isSwapped ? 5 : 70,
+      left: isSwapped ? 5 : 0,
+      duration: const Duration(milliseconds: 500),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        width: isSwapped ? 90 : screenWidth * 0.9,
+        height: isSwapped ? 120 : screenHeight * 0.6,
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage(
+              "assets/b64ac4c65983e8702efcc1024bf6b142-e1505106508525.jpg",
+            ),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: isSwapped
+              ? const BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                  bottomLeft: Radius.circular(25),
+                )
+              : BorderRadius.circular(30),
+          border: Border.all(
+            width: isSwapped ? 5 : 0,
+            color: isSwapped ? constants.backgroundColor : Colors.transparent,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSmallImage(double screenWidth, double screenHeight) {
+    return AnimatedPositioned(
+      top: isSwapped ? 70 : 5,
+      left: isSwapped ? 0 : 5,
+      duration: const Duration(milliseconds: 500),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            isSwapped = !isSwapped;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          width: isSwapped ? screenWidth * 0.9 : 90,
+          height: isSwapped ? screenHeight * 0.6 : 120,
+          decoration: BoxDecoration(
+            border: isSwapped
+                ? Border.all(width: 0, color: Colors.transparent)
+                : Border.all(width: 5, color: constants.backgroundColor),
+            image: const DecorationImage(
+              image: AssetImage(
+                "assets/michael-dam-mEZ3PoFGs_k-unsplash.jpg",
+              ),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: isSwapped
+                ? BorderRadius.circular(30)
+                : const BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                    bottomLeft: Radius.circular(25),
+                  ),
+          ),
+        ),
       ),
     );
   }
